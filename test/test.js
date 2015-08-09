6dceec8709a91372be7797e8bc252f89d5644f2c
Robert Keizer <robert@keizer.ca>
2015-08-09 13:48:25 -0500

    Adding in some options for elasticsearch.

--- test/test.js
+++ test/test.js
@@ -3,11 +3,41 @@
 var assert	= require( "assert" );
 var onionojs	= require( "../" );
 
+var _validOptions = { 
+	elasticsearch: {
+		host: "localhost",
+		port: 9200,
+		indexPrefix: "onionojs-"
+	}
+};
+
+describe( "Ensuring options", function( ){
+
+	it( "Throws an error if undefined is passed in", function( ){
+		assert.throws( function( ){
+			new onionojs.OnionoJS( );
+		} );
+	} );
+
+	it( "Fails with invalid options", function( ){
+
+		assert.throws( function( ){
+			new onionojs.OnionoJS( { "foo": "bar" } );
+		} );
+	} );
+
+	it( "Can be created with valid options", function( ){
+		new onionojs.OnionoJS( _validOptions );
+	} );
+
+} );
+
+/*
 describe( "GrabData", function( ){
 	var instance;
 
 	before( function( ){
-		instance = new onionojs.OnionoJS( );
+		instance = new onionojs.OnionoJS( { "foo": "bar" );
 	} );
 
 	after( function( ){
@@ -21,3 +51,4 @@ describe( "GrabData", function( ){
 	} );
 
 } );
+*/
